2e82f09fb4f8f3ee161e036e8caf6fc3f78399a2
surpriselizard <52936787+surpriselizard@users.noreply.github.com>
2019-08-13 13:48:11 -0600

    Adding new files

--- ReminderProjectWPF/MainWindow.xaml.cs
+++ ReminderProjectWPF/MainWindow.xaml.cs
@@ -24,5 +24,48 @@ namespace ReminderProjectWPF
         {
             InitializeComponent();
         }
+
+        private void NewReminder_Clicked(object sender, RoutedEventArgs e)
+        {
+            Window window = new Window();
+            StackPanel mainPanel = new StackPanel();
+            Label nameLabel = new Label();
+            Label descLabel = new Label();
+            Label dateLabel = new Label();
+            TextBox nameTxtbox = new TextBox();
+            TextBox descTxtbox = new TextBox();
+            DatePicker datePicker = new DatePicker();
+            ReminderDbManager dbManager = new ReminderDbManager();
+
+            window.Padding = new Thickness(10);
+            window.Title = "New Reminder";
+            window.Width = 300.0d;
+            window.Height = 500.0d;
+            window.Content = mainPanel;
+
+            nameLabel.Content = "Name: ";
+            nameTxtbox.Margin = new Thickness(7.0d, 0.3d, 7.0d, 0.3d);
+            nameTxtbox.MaxLength = 25;
+
+            descLabel.Content = "Description: ";
+            descTxtbox.Margin = new Thickness(7.0d, 0.3d, 7.0d, 0.3d);
+            descTxtbox.Height = 100.0d;
+            descTxtbox.TextWrapping = TextWrapping.Wrap;
+            descTxtbox.MaxLength = 140;
+
+            dateLabel.Content = "Choose the due date for the reminder: ";
+            datePicker.Margin = new Thickness(7.0d, 0.3d, 7.0d, 0.3d);
+
+            mainPanel.Children.Add(nameLabel);
+            mainPanel.Children.Add(nameTxtbox);
+
+            mainPanel.Children.Add(descLabel);
+            mainPanel.Children.Add(descTxtbox);
+
+            mainPanel.Children.Add(dateLabel);
+            mainPanel.Children.Add(datePicker);
+
+            window.Show();
+        }
     }
 }
---
+++ ReminderProjectWPF/ReminderDbManager.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReminderProjectWPF
+{
+    public class ReminderDbManager
+    {
+        private string _path = "";
+
+        public ReminderDbManager()
+        {
+            var list = new List<string>(Directory.GetCurrentDirectory().Split('\\'));
+
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                if (list[i] == "ReminderProjectWPF")
+                {
+                    list.Add("ReminderFile.txt");
+                    break;
+                }
+                else if (list[i] != "ReminderProjectWPF")
+                    list.Remove(list[i]);
+            }
+
+            _path = string.Join(@"\", list);
+
+
+            var reminders = new List<string>();
+            var readStream = File.Open(_path, FileMode.Open, FileAccess.Read, FileShare.None);
+            StreamReader reader;
+            reader = new StreamReader(readStream);
+            readStream.Dispose();
+
+            foreach (var line in File.ReadAllLines(_path))
+            {
+                reminders.Add(line);
+            }
+
+            foreach (var line in reminders)
+            {
+                var values = line.Split(new string[] { "*||*" }, StringSplitOptions.None);
+                var tempRem = new Reminder(Convert.ToDateTime(values[1]), values[0]);
+                tempRem.ReminderDesc = values[2];
+                ReminderDatabase.Add(tempRem);
+            }
+
+            reader.Close();
+        }
+
+        public List<Reminder> ReminderDatabase { get; private set; } = new List<Reminder>();
+
+        public void AddReminder(Reminder reminder)
+        {
+            ReminderDatabase.Add(reminder);
+
+            var writeStream = File.Open(_path, FileMode.Open, FileAccess.Write, FileShare.None);
+
+            var writer = new StreamWriter(writeStream);
+            writer.WriteLine($"{reminder.ReminderName}*||*{reminder.DateDue}*||*{reminder.ReminderDesc}");
+
+            writer.Close();
+        }
+
+        public void DeleteReminder(Reminder reminder)
+        {
+            var writeStream = File.Open(_path, FileMode.Open, FileAccess.Write, FileShare.None);
+            var writer = new StreamWriter(writeStream);
+            writer.Dispose();
+            writeStream.Dispose();
+
+            var tempFile = Path.GetTempFileName();
+            var linesToKeep = File.ReadLines(_path).Where(l => l != $"{reminder.ReminderName}*||*{reminder.DateDue}*||*{reminder.ReminderDesc}");
+
+            File.WriteAllLines(tempFile, linesToKeep);
+
+            File.Delete(_path);
+            File.Move(tempFile, _path);
+
+            writer.Close();
+        }
+
+        public List<List<Reminder>> RemindersIn7Days()
+        {
+            var RemsInWeek = new List<List<Reminder>>(7);
+            for (int i = 0; i < RemsInWeek.Capacity; ++i)
+            {
+                RemsInWeek.Add(new List<Reminder>());
+            }
+
+            foreach (var reminder in ReminderDatabase)
+            {
+                for (int i = 0; i < 7; ++i)
+                {
+                    if (reminder.DateDue.Date == DateTime.Today.AddDays(i))
+                    {
+                        RemsInWeek[i].Add(reminder);
+                    }
+                }
+            }
+
+            return RemsInWeek;
+        }
+
+        public List<List<Reminder>> RemInMonth()
+        {
+            var NumOfDaysInMonth = DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month);
+            var RemsInMonth = new List<List<Reminder>>(NumOfDaysInMonth);
+            var DaysInMonth = new List<DateTime>(NumOfDaysInMonth);
+
+            for (int i = 0; i < NumOfDaysInMonth; ++i)
+                RemsInMonth.Add(new List<Reminder>());
+            for (int i = 0; i < NumOfDaysInMonth; ++i)
+                DaysInMonth.Add(new DateTime(DateTime.Now.Year, DateTime.Now.Month, i + 1));
+
+            foreach (var reminder in ReminderDatabase)
+            {
+                for (int i = 0; i < NumOfDaysInMonth; ++i)
+                    if (reminder.DateDue.Date == DaysInMonth[i].Date)
+                        RemsInMonth[i].Add(reminder);
+            }
+
+            return RemsInMonth;
+        }
+    }
+}
