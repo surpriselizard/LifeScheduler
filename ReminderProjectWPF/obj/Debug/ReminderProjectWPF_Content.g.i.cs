0519ddce53153a60edf26f397fab9fe670100c58
surpriselizard <52936787+surpriselizard@users.noreply.github.com>
2019-08-15 13:41:21 -0600

    Some new changes.
    Fixing bugs with old ReminderDbManager Class.

--- ReminderProjectWPF/obj/Debug/ReminderProjectWPF_Content.g.i.cs
+++ ReminderProjectWPF/obj/Debug/ReminderProjectWPF_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("pictures/newreminder.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("reminderfile.txt")]
 
 
